37cb345512a42ea122b4891dc037c1bc709899dc
Atsushi Suzuki <atsushisuzuki.mail@gmail.com>
2013-01-24 00:03:04 +0900

    商品管理アプリケーションの作成

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = 'b4082c7b5ccdcfedc169ee26bc296c696def181ccbd1108e9d82d74842e9e147fe4a76d01be4438319a7c11e4e74b5aea1ab0e16bb55cf324b76d33b9fcdb744'
